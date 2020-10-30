b024b6c2e4fa61657ec246f4088385e2369f4996
Robert3141 <robert.aries1@gmail.com>
2020-10-30 16:17:09 +0000

    Almost fully working

--- lib/shared_preferences_moretypes.dart
+++ lib/shared_preferences_moretypes.dart
@@ -1,7 +1,111 @@
 library shared_preferences_moretypes;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+//imports
+import 'dart:core';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ExtendedPrefs {
+  Future dataStore(String key, dynamic value) async {
+    //get shared prefs
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String type = value.runtimeType.toString();
+    //print("store - key $key value $value");
+
+    //set pref based on type
+    if (type == "String") await prefs.setString(key, value);
+    if (type == "int") await prefs.setInt(key, value);
+    if (type == "double") await prefs.setDouble(key, value);
+    if (type == "bool") await prefs.setBool(key, value);
+    if (type.substring(0, 4) == "List") {
+      if (value.length > 0) {
+        type = value[0].runtimeType.toString();
+        if (type == "String")
+          await prefs.setStringList(key, value);
+        else if (type == "double" || type == "int" || type == "bool") {
+          List<String> raw = List.empty(growable: true);
+          for (int i = 0; i < value.length; i++) {
+            raw.add(type == "bool"
+                ? value[i]
+                    ? "1"
+                    : "0"
+                : value[i].toString());
+          }
+          await prefs.setStringList(key, raw);
+        } else {
+          type = value[0][0].runtimeType.toString();
+          for (int i = 0; i < value.length; i++) {
+            await dataStore("$key-$i", value[i]);
+          }
+          await prefs.setInt(key, value.length);
+        }
+      }
+    }
+  }
+
+  /*Future<dynamic> dataLoad(String key, Type type) async =>
+      _dataLoadText(key, type.toString());*/
+
+  Future<dynamic> dataLoad(String key, String type) async {
+    //get share prefs
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    //print("load - key $key type $type");
+
+    //set pref based on type
+    if (type == "String")
+      return prefs.getString(key);
+    else if (type == "int")
+      return prefs.getInt(key);
+    else if (type == "double")
+      return prefs.getDouble(key);
+    else if (type == "bool")
+      return prefs.getBool(key);
+    else if (type.substring(0, 4) == "List") {
+      type = type.substring(5, type.length - 1);
+      if (type == "String")
+        return prefs.getStringList(key);
+      else if (type == "double" || type == "int" || type == "bool") {
+        List<String> raw = prefs.getStringList(key);
+        if (type == "double") {
+          List<double> data = List.empty(growable: true);
+          for (int i = 0; i < raw.length; i++) {
+            data.add(double.parse(raw[i]));
+          }
+          return data;
+        }
+        if (type == "int") {
+          List<int> data = List.empty(growable: true);
+          for (int i = 0; i < raw.length; i++) {
+            data.add(int.parse(raw[i]));
+          }
+          return data;
+        }
+        if (type == "bool") {
+          List<bool> data = List.empty(growable: true);
+          for (int i = 0; i < raw.length; i++) {
+            data.add(raw[i] == "1");
+          }
+          return data;
+        }
+      } else if (type.split("<").length > 1) {
+        List<dynamic> data = List.empty(growable: true);
+        int length = prefs.getInt(key);
+        //find next <
+        for (int i = 0; i < type.length - 1; i++) {
+          if (type[i] == "<") {
+            type = "List<" + type.substring(i + 1);
+          }
+        }
+        //type = type.split("<")[1];
+        //type = type.substring(0, type.length - 1);
+        for (int i = 0; i < length; i++) {
+          data.add(await dataLoad("$key-$i", type));
+        }
+        return data;
+      } else
+        return null;
+    } else
+      return null;
+  }
 }
--- test/shared_preferences_moretypes_test.dart
+++ test/shared_preferences_moretypes_test.dart
@@ -1,13 +1,53 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:shared_preferences_moretypes/shared_preferences_moretypes.dart';
 
-void main() {
-  test('adds one to input values', () {
+void main() async {
+  /*test('adds one to input values', () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });*/
+
+  var prefs = ExtendedPrefs();
+
+  test("String", () async {
+    await prefs.dataStore("keyS", "value");
+    expect(await prefs.dataLoad("keyS", "String"), "value");
+  });
+  test("int", () async {
+    await prefs.dataStore("keyI", 3);
+    expect(await prefs.dataLoad("keyI", "int"), 3);
+  });
+  test("bool", () async {
+    await prefs.dataStore("keyB", true);
+    expect(await prefs.dataLoad("keyB", "bool"), true);
+  });
+  test("double", () async {
+    await prefs.dataStore("keyD", 4.7);
+    expect(await prefs.dataLoad("keyD", "double"), 4.7);
+  });
+  test("List<String>", () async {
+    await prefs.dataStore("keyLS", ["value1", "value2", "value3"]);
+    expect(await prefs.dataLoad("keyLS", "List<String>"),
+        ["value1", "value2", "value3"]);
+  });
+  test("List<double>", () async {
+    await prefs.dataStore("keyLD", [3.0, 7.2, 56.5]);
+    expect(await prefs.dataLoad("keyLD", "List<double>"), [3.0, 7.2, 56.5]);
+  });
+
+  test("List<List<double>>", () async {
+    await prefs.dataStore("keyLLD", [
+      [3.0, 7.2, 56.4],
+      [43.4, 3242.4, 234.5],
+      [434.3343434, 343434.333333, 444455.3333]
+    ]);
+    expect(await prefs.dataLoad("keyLLD", "List<double<double>>"), [
+      [3.0, 7.2, 56.4],
+      [43.4, 3242.4, 234.5],
+      [434.3343434, 343434.333333, 444455.3333]
+    ]);
   });
 }
